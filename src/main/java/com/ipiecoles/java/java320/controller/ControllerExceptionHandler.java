78c4857e315120f71b131ca643b23848b84f6a2f
pjvilloud <pjvilloud@protonmail.com>
2020-10-10 12:03:40 +0200

    Ajout Erreur 404 pour get employe/id

---
+++ src/main/java/com/ipiecoles/java/java320/controller/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.ipiecoles.java.java320.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView handleEntityNotFoundException(EntityNotFoundException e){
+        ModelAndView modelAndView = new ModelAndView("main");
+        modelAndView.addObject("error", e.getMessage());
+        modelAndView.addObject("status", HttpStatus.NOT_FOUND.value());
+        modelAndView.addObject("page", "error");
+        modelAndView.addObject("fragment", "error");
+        return modelAndView;
+    }
+
+}
--- src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/java320/controller/EmployeController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.time.LocalDate;
 
 @Controller
 public class EmployeController {
